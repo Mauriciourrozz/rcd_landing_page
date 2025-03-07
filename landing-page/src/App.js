70c5fa7310232723a452f4a0b7a0de77550b553e
Mauriciourrozz <mauriciourrozz@gmail.com>
2025-03-07 14:59:23 -0300

    Update style

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -4,26 +4,26 @@ import './App.css';
 function Card({ frontContent, backContent }) {
   const [flipped, setFlipped] = useState(false);
 
-return (
-  <div 
-    className={`card ${flipped ? 'flipped' : ''}`} 
-    onClick={() => setFlipped(!flipped)}
-  >
-    <div className="card-inner">
-      <div className="card-front">
-        {frontContent}
-      </div>
-      <div className="card-back">
-        {backContent}
+  return (
+    <div 
+      className={`card ${flipped ? 'flipped' : ''}`} 
+      onClick={() => setFlipped(!flipped)}
+    >
+      <div className="card-inner">
+        <div className="card-front">
+          {frontContent}
+        </div>
+        <div className="card-back">
+          {backContent}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <header className="header">
         <nav className="header-nav">
           <a href="#section1" className="nav-link">Home</a>
@@ -32,32 +32,30 @@ function App() {
         </nav>
       </header>
 
-{/* -------------------------------------------------------------------------------------------------------------------------------------------- */}
-
+      {/* -------------------------------------------------------------------------------------------------------------------------------------------- */}
       <section id="section1" className="section1">
-        <h2 className="section1-title">Sistema de gestion RCD Reciclaje</h2>
+        <h2 className="section1-title">Sistema de gestión RCD Reciclaje</h2>
         <p className="section1-text">
-          Gestiona los residuos de tus obras dando click al siguiente boton
+          Gestiona los residuos de tus obras dando click al siguiente botón
         </p>
-        <button className="section1-button">Registrate aqui</button>
-        <p className="section1-extra">Nues</p>
+        <button className="section1-button">Regístrate aquí</button>
+        <p className="section1-extra">Aquí va un buen chamuyo de la app</p>
       </section>
 
-{/* -------------------------------------------------------------------------------------------------------------------------------------------- */}
-
-<section id="section2" className="section2">
-        <h2 className="section2-title">Simple y facil de usar</h2>
+      {/* -------------------------------------------------------------------------------------------------------------------------------------------- */}
+      <section id="section2" className="section2">
+        <h2 className="section2-title">Simple y fácil de usar</h2>
 
         <div className="section2-row row1">
-          <p className="section2-text left-text">Registrate e inicia sesion</p>
-            <div className="section2-image-placeholder">
-            {/* Aquí se colocará la imagen (ruta a definir) */}
-            </div>
+          <p className="section2-text left-text">Regístrate e inicia sesión</p>
+          <div className="section2-image-placeholder">
+            <img src="/images/Iniciodesesion.PNG" alt="Regístrate e inicia sesión" />
+          </div>
         </div>
 
         <div className="section2-row row2">
           <div className="section2-image-placeholder">
-            {/* Aquí se colocará la imagen (ruta a definir) */}
+            <img src="/images/registroobra1.PNG" alt="Completa el formulario" />
           </div>
           <p className="section2-text right-text">Completa un formulario y registra tus obras</p>
         </div>
@@ -65,15 +63,15 @@ function App() {
         <div className="section2-row row3">
           <p className="section2-text left-text">Solicita el retiro de tus escombros</p>
           <div className="section2-image-placeholder">
-            {/* Aquí se colocará la imagen (ruta a definir) */}
+            <img src="/images/coordinacion.PNG" alt="Solicita el retiro de escombros" />
           </div>
         </div>
       </section>
 
-{/* -------------------------------------------------------------------------------------------------------------------------------------------- */}
-
-<section id="section3" className="section3">
+      {/* -------------------------------------------------------------------------------------------------------------------------------------------- */}
+      <section id="section3" className="section3">
         <h2 className="section3-title">Nuestro equipo</h2>
+        <p className="linkedin-text">Haz click en las imágenes para acceder a nuestro perfil de LinkedIn</p>
         <div className="cards-container">
           {/* Tarjeta 1 */}
           <Card
@@ -90,10 +88,12 @@ function App() {
             }
             backContent={
               <>
-                {/* Contenido del reverso de la tarjeta (por ejemplo, otra imagen o información adicional) */}
                 <div className="card-back-content">
                   <img src="/images/Nachoqr.png" alt="Ignacio Vanzo Back" />
                 </div>
+                <a href="https://github.com/nachovanzo7" target="_blank" rel="noopener noreferrer" className="card-logo-link">
+                  <img src="/images/github.png" alt="Logo" className="card-logo" />
+                </a>
               </>
             }
           />
@@ -116,6 +116,9 @@ function App() {
                 <div className="card-back-content">
                   <img src="/images/Mauriqr.png" alt="Mauricio Urroz Back" />
                 </div>
+                <a href="https://github.com/Mauriciourrozz" target="_blank" rel="noopener noreferrer" className="card-logo-link">
+                  <img src="/images/github.png" alt="Logo" className="card-logo" />
+                </a>
               </>
             }
           />
@@ -138,6 +141,9 @@ function App() {
                 <div className="card-back-content">
                   <img src="/images/OwenFernandez-back.jpg" alt="Owen Fernandez Back" />
                 </div>
+                <a href="https://github.com/OwenFernandez9" target="_blank" rel="noopener noreferrer" className="card-logo-link">
+                  <img src="/images/github.png" alt="Logo" className="card-logo" />
+                </a>
               </>
             }
           />
